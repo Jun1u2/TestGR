ee1f5b3633d2239252e196acb3f9a40f5bd2a796
Jun1u2 <114225326+Jun1u2@users.noreply.github.com>
2023-12-01 14:31:33 +0700

    Update login.php

--- login.php
+++ login.php
@@ -36,11 +36,11 @@ if( isset( $_POST[ 'Login' ] ) ) {
 		dvwaRedirect( DVWA_WEB_PAGE_TO_ROOT . 'setup.php' );
 	}
 
-	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
+	$query  = "SELECT * FROM `users` WHERE user='$userasd' AND password='$pass';";
 	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 	if( $result && mysqli_num_rows( $result ) == 1 ) {    // Login Successful...
-		dvwaMessagePush( "You have logged in as '{$user}'" );
-		dvwaLogin( $user );
+		dvwaMessagePush( "You have logged in as '{$userasd}'" );
+		dvwaLogin( $userasd );
 		dvwaRedirect( DVWA_WEB_PAGE_TO_ROOT . 'index.php' );
 	}
 
